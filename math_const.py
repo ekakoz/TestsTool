bfafbe3d2fc74a3b49423424449a3e6fcaaf9825
Katrin <kozlova.ekaterinaa@gmail.com>
2012-05-02 15:16:18 +0400

    sampler module with function

--- math_const.py
+++ math_const.py
@@ -28,37 +28,37 @@ def math_const(str):#*args, **kwargs):
 
 
 
-
-def interrogate(item):
-    """Print useful information about item."""
-    print __name__
-    print '+++++++++++++'
-    if hasattr(item, '__name__'):
-        print "NAME:\t\t", item.__name__
-    if hasattr(item, '__class__'):
-        print "CLASS:\t\t", item.__class__.__name__
-    print "ID:\t\t\t", id(item)
-    print "TYPE:\t\t", type(item)
-    print "VALUE:\t\t", repr(item)
-    if str(type(item)) == "<type 'function'>":
-        print "ARGSPEC:\t", inspect.getargspec(item)
-    print "CALLABLE:\t",
-
-    if callable(item):
-        print "Yes"
-    else:
-        print "No"
-
-#    f = inspect.currentframe()
- #   print inspect.getargvalues(f)
-
-if __name__ == '__main__':
-
-    #interrogate(3)
-    #print __file__
-    #print interrogate(3)
-    #print dir(interrogate)
-    #print 'math_const(\'pi:4\')'
-    math_const('pi:4')
-    math_const('e:19')
-    math_const('er:19')
+#
+#def interrogate(item):
+#    """Print useful information about item."""
+#    print __name__
+#    print '+++++++++++++'
+#    if hasattr(item, '__name__'):
+#        print "NAME:\t\t", item.__name__
+#    if hasattr(item, '__class__'):
+#        print "CLASS:\t\t", item.__class__.__name__
+#    print "ID:\t\t\t", id(item)
+#    print "TYPE:\t\t", type(item)
+#    print "VALUE:\t\t", repr(item)
+#    if str(type(item)) == "<type 'function'>":
+#        print "ARGSPEC:\t", inspect.getargspec(item)
+#    print "CALLABLE:\t",
+#
+#    if callable(item):
+#        print "Yes"
+#    else:
+#        print "No"
+#
+##    f = inspect.currentframe()
+# #   print inspect.getargvalues(f)
+#
+#if __name__ == '__main__':
+#
+#    #interrogate(3)
+#    #print __file__
+#    #print interrogate(3)
+#    #print dir(interrogate)
+#    #print 'math_const(\'pi:4\')'
+#    math_const('pi:4')
+#    math_const('e:19')
+#    math_const('er:19')
